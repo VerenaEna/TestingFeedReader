b1f2b63010b78a0e62b89b496f4b36cffb04d63e
Verena Ena <verenaparaluxe@gmail.com>
2018-05-23 14:50:32 +0200

    Feat: add first tests for RSS Feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -23,7 +23,7 @@ $(function() {
    * allFeeds in app.js to be an empty array and refresh the
    * page?
    */
-		it('are defined', function() {
+		it('feeds are defined', function() {
 			expect(allFeeds).toBeDefined();
 			expect(allFeeds.length).not.toBe(0);
 		});
@@ -33,12 +33,25 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+		it('urls are defined', function(){
+			for(var i = 0; i < allFeeds.length; i++){
+				expect(allFeeds[i].url).toBeDefined();
+				expect(allFeeds[i].url).not.toBe(0);
+			}
+		});
 
 
 		/* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+		it('name are defined', function(){
+			for(var i = 0; i < allFeeds.length; i++){
+				expect(allFeeds[i].name).toBeDefined();
+				expect(allFeeds[i].name).not.toBe(0);
+			}
+		});
 	});
 
 
