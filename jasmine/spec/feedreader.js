cabdfda5addbfdaae132bd992c1a693377fbb759
Verena Ena <verenaparaluxe@gmail.com>
2018-05-24 12:32:06 +0200

    Feat: Add new test suite inital entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,15 +79,27 @@ $(function() {
 			expect($('body').hasClass('menu-hidden')).toBe(true);
 		});
 	});
+
 	/* TODO: Write a new test suite named "Initial Entries" */
+	describe('Initial Entries', function(){
 
-	/* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+		beforeEach(function(done) {
+			loadFeed(0,function(){
+				done();
+			});
+		});
 
+		/* TODO: Write a test that ensures when the loadFeed
+					 * function is called and completes its work, there is at least
+					 * a single .entry element within the .feed container.
+					 * Remember, loadFeed() is asynchronous so this test will require
+					 * the use of Jasmine's beforeEach and asynchronous done() function.
+					 */
+	
+		it('at least a single entry in container', function(){
+			expect($('.entry').length).toBeGreaterThan(0);
+		});
+	});
 	/* TODO: Write a new test suite named "New Feed Selection" */
 
 	/* TODO: Write a test that ensures when a new feed is loaded
